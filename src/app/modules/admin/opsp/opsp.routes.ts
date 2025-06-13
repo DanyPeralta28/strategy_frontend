cb22b9422a54185302bcbcd9d6db7c3b724da72b
Dany Peralta <danyperalta28@yahoo.com.mx>
2025-06-13 00:20:07 -0600

    Subiendo routes

--- src/app/modules/admin/opsp/opsp.routes.ts
+++ src/app/modules/admin/opsp/opsp.routes.ts
@@ -5,10 +5,20 @@ import { VisionComponent } from 'app/modules/admin/opsp/formats/vision/vision.co
 import { StrataComponent } from 'app/modules/admin/opsp/formats/strata/strata.component';
 import { FdtComponent } from 'app/modules/admin/opsp/formats/fdt/fdt.component';
 import { FactorxComponent } from 'app/modules/admin/opsp/formats/factorx/factorx.component';
+import { BalancekpisComponent } from 'app/modules/admin/opsp/formats/balancekpis/balancekpis.component';
+import { UtilidadxComponent } from 'app/modules/admin/opsp/formats/utilidadx/utilidadx.component';
+import { GoalsComponent } from 'app/modules/admin/opsp/formats/goals/goals.component';
+import { CorevaluesComponent } from 'app/modules/admin/opsp/formats/corevalues/corevalues.component';
+import { PurposeComponent } from 'app/modules/admin/opsp/formats/purpose/purpose.component';
+import { KeycompetenciesComponent } from 'app/modules/admin/opsp/formats/keycompetencies/keycompetencies.component';
+import { TerritoryComponent } from 'app/modules/admin/opsp/formats/territory/territory.component';
+import { CultureComponent } from 'app/modules/admin/opsp/formats/culture/culture.component';
+import { CentralclientComponent } from 'app/modules/admin/opsp/formats/centralclient/centralclient.component';
+import { BrandpromisesComponent } from 'app/modules/admin/opsp/formats/brandpromises/brandpromises.component';
 
 export default [
     {
-        path     : '',
+        path: '',
         pathMatch: 'full',
         component: OpspComponent,
     },
@@ -20,16 +30,56 @@ export default [
         path: 'formats/vision',
         component: VisionComponent
     },
-        {
+    {
         path: 'formats/strata',
         component: StrataComponent
     },
-        {
+    {
         path: 'formats/fdt',
         component: FdtComponent
     },
-        {
+    {
         path: 'formats/factorx',
         component: FactorxComponent
+    },
+    {
+        path: 'formats/balancekpis',
+        component: BalancekpisComponent
+    },
+    {
+        path: 'formats/utilidadx',
+        component: UtilidadxComponent
+    },
+    {
+        path: 'formats/goals',
+        component: GoalsComponent
+    },
+    {
+        path: 'formats/corevalues',
+        component: CorevaluesComponent
+    },
+    {
+        path: 'formats/purpose',
+        component: PurposeComponent
+    },
+    {
+        path: 'formats/keycompetencies',
+        component: KeycompetenciesComponent
+    },
+    {
+        path: 'formats/territory',
+        component: TerritoryComponent
+    },
+    {
+        path: 'formats/culture',
+        component: CultureComponent
+    },
+    {
+        path: 'formats/centralclient',
+        component: CentralclientComponent
+    },
+    {
+        path: 'formats/brandpromises',
+        component: BrandpromisesComponent
     }
 ] as Routes;
